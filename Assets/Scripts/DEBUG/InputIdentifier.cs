fe8cb8b578aa9b6b45864549435b68b69041e06a
bastingup <Cattoaster@outlook.de>
2019-01-26 16:35:33 +0100

    Fixed Second Cam

---
+++ Assets/Scripts/DEBUG/InputIdentifier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputIdentifier : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log(Input.inputString);
+        }
+    }
+}
