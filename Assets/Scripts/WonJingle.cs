0a34db3fef10a702ce1d79e38d436b7b3d9d311b
bastingup <Cattoaster@outlook.de>
2019-01-27 16:42:57 +0100

    Added jingle to end screen

---
+++ Assets/Scripts/WonJingle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WonJingle : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip jingle;
+    private AudioSource audio;
+
+    void Start()
+    {
+        SetUp();
+        audio.PlayOneShot(jingle);
+    }
+
+    void SetUp()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+}
